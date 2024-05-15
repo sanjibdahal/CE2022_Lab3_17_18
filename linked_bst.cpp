5c08a4a837e3605998f6defb0ae07c9221c7d896
Sanjib Dahal <sanjibdahal04@gmail.com>
2024-05-15 10:42:15 +0545

    linkedBST implementation

---
+++ linked_bst.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "linked_bst.h"
+
+using namespace std;
+
+LinkedBST::LinkedBST() : root(nullptr) {}
+LinkedBST::~LinkedBST() {}
+
+bool LinkedBST::isEmpty()
+{
+    return root == nullptr;
+}
+
+void LinkedBST::addBST(int data)
+{
+    
+}
+
+bool LinkedBST::removeBST(int keyToDelete)
+{
+
+}
+
+bool LinkedBST::searchBST(int targetKey)
+{
+
+}
---
+++ linked_bst.h
@@ -0,0 +1,23 @@
+#include "bst.h"
+
+class Node {
+    public:
+        int key;
+        Node* leftChild;
+        Node* rightChild;
+        Node() {}
+        Node(int d) : key(d), leftChild(nullptr), rightChild(nullptr) {}
+        Node(int d, Node *leftChild, Node *rightChild) : key(d),leftChild(leftChild), rightChild(rightChild) {}
+};
+
+class LinkedBST : public BST {
+    private:
+        Node *root;
+    public:
+        LinkedBST();
+        ~LinkedBST() {};
+        bool isEmpty();
+        void addBST(int data);
+        bool removeBST(int keyToDelete);
+        bool searchBST(int targetKey);
+};
