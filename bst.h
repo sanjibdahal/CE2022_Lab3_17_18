4670d91f734ed9fb05ed5267b516670f19de7664
ac <sachinsrestha934@gmail.com>
2024-05-15 09:51:10 +0545

    second

--- bst.h
+++ bst.h
@@ -1,16 +1,8 @@
-class Node{
-    public:
-        Node(int key) : key(key), leftChild(nullptr), rightChild(nullptr) {}
-        int key;
-        Node* leftChild;
-        Node* rightChild;
-};
-
 class BST{
     public:
         virtual bool isEmpty() = 0;
-        virtual void addBST(Node* newNode) = 0;
-        virtual void removeBST(int keyToDelete) = 0;
+        virtual void addBST(int data) = 0;
+        virtual bool removeBST(int keyToDelete) = 0;
         virtual bool searchBST(int targetKey) = 0;
 };
 
